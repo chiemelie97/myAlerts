11bc6fa9af4aa3d0de029583f56d243af28ca00d
Chiemelie Ikwukeme <46831024+chiemelie97@users.noreply.github.com>
2021-03-21 00:55:16 +0000

    Update db connection

--- myCon.php
+++ myCon.php
@@ -2,12 +2,15 @@
 
 $password="Akuoma.96";
 $user = "root";
-$webserver="172.17.0.2:3306";
-$db = "myAlertsApp";   
+$webserver="127.0.0.1";
+$db = "myAlertsApp";
 
 $conn = new mysqli($webserver, $user, $password, $db);  
 
 if($conn->connect_error){
     echo "Connection failed: ".$conn->connect_error;
+} else {
+    
+    echo "IT WORKED";
 }
 ?>
